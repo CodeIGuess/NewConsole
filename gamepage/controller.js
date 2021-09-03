fa425b2944e3f9dcdc6274e436d63f1eef7fa489
CodeIGuess <codeiguess@gmail.com>
2021-09-02 22:51:25 -0400

    Checking if touch is or is not used
    This is useful for when `onpointerup` isn't called.

--- gamepage/controller.js
+++ gamepage/controller.js
@@ -1,12 +1,15 @@
 
 console.log("Loaded controller!")
 
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
 function buttonPressed(i) {
-    console.log("Pressed")
+    // console.log("Pressed")
     socket.send(ip + ":BD:" + i)
 }
 
-function buttonReleased(i) {
-    console.log("Released")
+function buttonReleased(i, pointerLeave) {
+    // console.log("Released")
+    if (pointerLeave && !isMobile) return
     socket.send(ip + ":BU:" + i)
 }
