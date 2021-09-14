9510ee73df19b4642eb07592553dd0e223bc9db1
CodeIGuess <codeiguess@gmail.com>
2021-09-14 16:40:29 -0400

    Made the display *work*

--- gamepage/display.js
+++ gamepage/display.js
@@ -14,21 +14,26 @@ socket.addEventListener('message', function (event) {
             // console.log("KEYFRAME!")
             cnt = cnt.split(',')
             let id = parseInt(cnt.splice(0, 1))
-            let tag = document.createElement(cnt.splice(0, 1))
-            if (display.children.length <= id) display.appendChild(tag)
+            let tag = document.createElementNS("http://www.w3.org/2000/svg", cnt.splice(0, 1))
             cnt = cnt.map(e => e.split(":"))
             console.log(cnt)
             for (let a = 0; a < cnt.length; a++) {
-                tag.setAttribute(cnt[a][0], cnt[a][1])
+                if (cnt[a][0] == "fill") {
+                    tag.setAttributeNS(null, "style", cnt[a][0] + ":var(" + cnt[a][1] + ")")
+                } else {
+                    tag.setAttributeNS(null, cnt[a][0], cnt[a][1])
+                }
             }
+            console.log("Child:", display.children, id)
+            if (display.children.length <= id) display.appendChild(tag)
         } break
         case "GP": {
-            // console.log(cnt)
-            // let fullTag = cnt.split(',').map(e => e.split(':'))
-            // let tag = display.children[fullTag[0]]
-            // for (let a = 1; a < fullTag.length; a++) {
-            //     tag.setAttribute(fullTag[a][0], fullTag[a][1])
-            // }
+            console.log(cnt)
+            let fullTag = cnt.split(',').map(e => e.split(':'))
+            let tag = display.children[fullTag[0]]
+            for (let a = 1; a < fullTag.length; a++) {
+                tag.setAttribute(fullTag[a][0], fullTag[a][1])
+            }
         } break
         default: {
             console.log(`Didn't understand packet: "${event.data}"`)
