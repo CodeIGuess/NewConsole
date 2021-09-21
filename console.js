27d7dc7bff2b7cc31f4b59bb7036ce7029c13feb
CodeIGuess <codeiguess@gmail.com>
2021-09-21 15:23:55 -0400

    Reading story data

--- console.js
+++ console.js
@@ -1,12 +1,26 @@
 
 class Console {
-    constructor() {
-        console.log("Initialized console")
+    constructor(id) {
+        if (id == undefined) {
+            console.log("Please input a console ID.")
+            id = -1
+        }
+        this.id = id   // Console ID
+        this.vars = {} // Game variables
+        this.on = 0    // On instruction number
+        console.log("Initialized console", id)
     }
 
     setStory(storyData) {
-        this.st
+        this.st = storyData.split("\n")
+        this.on = 0
+    }
+    
+    step() {
+        let ret = null
+        this.on++
+        return ret
     }
 }
 
-module.exports = { Console }
+module.exports = Console
